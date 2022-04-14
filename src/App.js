0ddfcc238801e36d39ad8f1b231d2e0dfef83d6c
Michel Brousse <mbweb47@gmail.com>
2022-04-14 12:56:58 +0200

    ✨ Implement routing and create matching pages with functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Fragment } from 'react'
 import GlobalStyle from './utils/styles/GlobalStyle'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/'
+import Employees from './pages/Employees/'
+import Error from './pages/Error/'
 
 function App() {
   return (
     <Fragment>
       <GlobalStyle />
-      <div>Hello Projet 14</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="employees" element={<Employees />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </Fragment>
   )
 }
